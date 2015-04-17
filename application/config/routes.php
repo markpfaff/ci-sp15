1cf851f514028ffc43a60234fe7e5000f9319697
markdavid <markdpfaff@gmail.com>
2015-04-16 17:54:54 -0700

    Adds News section Part of CodeIgniter Tutorial

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,5 +54,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$route['404_override'] = '';
 //$route['translate_uri_dashes'] = FALSE;
 
-$route['default_controller'] = 'pages/view';
+//2nd tutorial replacement
+//$route['default_controller'] = 'pages/view';
+//$route['(:any)'] = 'pages/view/$1';
+
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
